f96473e499180d6e9fe8c202b5b7d1ce0d149095
EpicSquid <epicsquid315@gmail.com>
2018-07-18 21:43:15 +1000

    Recipe Pass

--- src/main/java/epicsquid/mysticallib/recipe/RecipeRegistry.java
+++ src/main/java/epicsquid/mysticallib/recipe/RecipeRegistry.java
@@ -1,5 +1,6 @@
 package epicsquid.mysticallib.recipe;
 
+import javax.annotation.Nonnull;
 
 import epicsquid.mysticallib.LibRegistry;
 import epicsquid.mysticallib.event.RegisterModRecipesEvent;
@@ -14,24 +15,28 @@ import net.minecraftforge.oredict.ShapelessOreRecipe;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class RecipeRegistry {
-  public static ResourceLocation getRL(String s) {
+
+  public static ResourceLocation getRL(@Nonnull String s) {
     return new ResourceLocation(LibRegistry.getActiveModid() + ":" + s);
   }
 
-  public static void registerShaped(IForgeRegistry<IRecipe> registry, String name, ItemStack result, Object... ingredients) {
+  public static void registerShaped(@Nonnull IForgeRegistry<IRecipe> registry, @Nonnull String name, @Nonnull ItemStack result,
+      @Nonnull Object... ingredients) {
     registry.register(new ShapedOreRecipe(getRL(name), result, ingredients).setRegistryName(getRL(name)));
   }
 
-  public static void registerShapedMirrored(IForgeRegistry<IRecipe> registry, String name, ItemStack result, Object... ingredients) {
+  public static void registerShapedMirrored(@Nonnull IForgeRegistry<IRecipe> registry, @Nonnull String name, @Nonnull ItemStack result,
+      @Nonnull Object... ingredients) {
     registry.register(new ShapedOreRecipe(getRL(name), result, ingredients).setMirrored(true).setRegistryName(getRL(name)));
   }
 
-  public static void registerShapeless(IForgeRegistry<IRecipe> registry, String name, ItemStack result, Object... ingredients) {
+  public static void registerShapeless(@Nonnull IForgeRegistry<IRecipe> registry, @Nonnull String name, @Nonnull ItemStack result,
+      @Nonnull Object... ingredients) {
     registry.register(new ShapelessOreRecipe(getRL(name), result, ingredients).setRegistryName(getRL(name)));
   }
 
   @SubscribeEvent
-  public void onRegisterRecipes(RegistryEvent.Register<IRecipe> event) {
+  public void onRegisterRecipes(@Nonnull RegistryEvent.Register<IRecipe> event) {
     MinecraftForge.EVENT_BUS.post(new RegisterModRecipesEvent(event.getRegistry()));
   }
 }
