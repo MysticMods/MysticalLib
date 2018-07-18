f8857f03a750715e3fe9f86e50affb96556cc32a
EpicSquid <epicsquid315@gmail.com>
2018-07-18 20:27:23 +1000

    Temporarily removed container code

--- src/main/java/epicsquid/mysticallib/container/ContainerModular.java
+++ src/main/java/epicsquid/mysticallib/container/ContainerModular.java
@@ -1,17 +1,27 @@
 package epicsquid.mysticallib.container;
 
 import javax.annotation.Nonnull;
+import javax.swing.plaf.basic.BasicComboBoxUI;
 
 import epicsquid.mysticallib.tile.TileModular;
+import epicsquid.mysticallib.tile.module.IModule;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.SlotItemHandler;
 
+// FIXME: This is just spaghetti
 public class ContainerModular extends Container {
   public @Nonnull TileModular tile;
-  private @Nonnull IExtendedInventory inventory;
+  private @Nonnull ItemStackHandler inventory;
 
   @Override
   public boolean canInteractWith(@Nonnull EntityPlayer playerIn) {
@@ -20,35 +30,27 @@ public class ContainerModular extends Container {
 
   public ContainerModular(@Nonnull TileModular tile) {
     super();
-    inventory = new DummyInventory();
-    for (Module m : tile.modules.values()) {
-      if (m instanceof IExtendedInventory) {
-        inventory = (IExtendedInventory) m;
-      }
+    this.tile = tile;
+    if (tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)) {
+      inventory = (ItemStackHandler) tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+    } else {
+      inventory = new ItemStackHandler();
     }
   }
 
   @Nonnull
   public ContainerModular tryAddSlot(int index, int x, int y) {
-    if (index < inventory.getSizeInventory()) {
-      this.addSlotToContainer(new SlotInventoryDefault(inventory, index, x, y, false));
+    if (index < inventory.getSlots()) {
+      this.addSlotToContainer(new SlotItemHandler(inventory, index, x, y));
     }
     return this;
   }
 
-  @Nonnull
-  public ContainerModular tryAddSlot(int index, int x, int y, boolean big) {
-    if (index < inventory.getSizeInventory()) {
-      this.addSlotToContainer(new SlotInventoryDefault(inventory, index, x, y, big));
-    }
-    return this;
-  }
-
-  public void initContainerSlots(IInventory containerInv) {
+  public void initContainerSlots(@Nonnull IItemHandler inv) {
 
   }
 
-  public ContainerModular initPlayerInventory(IInventory playerInv, int offX, int offY) {
+  public ContainerModular initPlayerInventory(@Nonnull InventoryPlayer playerInv, int offX, int offY) {
     for (int y = 0; y < 3; ++y) {
       for (int x = 0; x < 9; ++x) {
         this.addSlotToContainer(new Slot(playerInv, x + y * 9 + 9, offX + 8 + x * 18, offY + 84 + y * 18));
@@ -61,46 +63,46 @@ public class ContainerModular extends Container {
     return this;
   }
 
-  @Override
-  @Nonnull
-  public ItemStack transferStackInSlot(@Nonnull EntityPlayer p, int i) {
-    ItemStack itemstack = ItemStack.EMPTY;
-    Slot slot = (Slot) inventorySlots.get(i);
-    if (i >= slot.inventory.getSizeInventory()) {
-      return itemstack;
-    }
-    if (slot != null && slot.getHasStack()) {
-      if (slot.inventory instanceof IExtendedInventory && i < slot.inventory.getSizeInventory() && !((IExtendedInventory) slot.inventory)
-          .canExtractFromSlot(i)) {
-        return itemstack;
-      }
-      ItemStack itemstack1 = slot.getStack();
-      itemstack = itemstack1.copy();
-      if (i < inventory.getSizeInventory()) {
-        if (!mergeItemStack(itemstack1, inventory.getSizeInventory(), inventorySlots.size(), false) && inventory.isItemValidForSlot(i, itemstack1) && inventory
-            .canInsertToSlot(i)) {
-          return ItemStack.EMPTY;
-        }
-      } else {
-        boolean isAllowed = false;
-        for (int j = inventory.getSizeInventory() - 1; j >= 0; j--) {
-          if (inventory.isItemValidForSlot(j, itemstack1) && inventory.canInsertToSlot(j)) {
-            isAllowed = true;
-          }
-        }
-        if (!isAllowed) {
-          return ItemStack.EMPTY;
-        } else if (!mergeItemStack(itemstack1, 0, inventory.getSizeInventory(), false)) {
-          return ItemStack.EMPTY;
-        }
-      }
-      if (itemstack1.getCount() == 0) {
-        slot.putStack(ItemStack.EMPTY);
-      } else {
-        slot.onSlotChanged();
-      }
-    }
-    return itemstack;
-  }
+  // TODO Move this to capability if/when it becomes relevant
+//  @Override
+//  @Nonnull
+//  public ItemStack transferStackInSlot(@Nonnull EntityPlayer p, int i) {
+//    ItemStack itemstack = ItemStack.EMPTY;
+//    Slot slot = inventorySlots.get(i);
+//    if (i >= slot.inventory.getSizeInventory()) {
+//      return itemstack;
+//    }
+//    if (slot.getHasStack()) {
+//      if (i < slot.inventory.getSizeInventory()) {
+//        return itemstack;
+//      }
+//      ItemStack itemstack1 = slot.getStack();
+//      itemstack = itemstack1.copy();
+//      if (i < inventory.getSizeInventory()) {
+//        if (!mergeItemStack(itemstack1, inventory.getSizeInventory(), inventorySlots.size(), false) && inventory.isItemValidForSlot(i, itemstack1) && inventory
+//            .canInsertToSlot(i)) {
+//          return ItemStack.EMPTY;
+//        }
+//      } else {
+//        boolean isAllowed = false;
+//        for (int j = inventory.getSizeInventory() - 1; j >= 0; j--) {
+//          if (inventory.isItemValidForSlot(j, itemstack1) && inventory.canInsertToSlot(j)) {
+//            isAllowed = true;
+//          }
+//        }
+//        if (!isAllowed) {
+//          return ItemStack.EMPTY;
+//        } else if (!mergeItemStack(itemstack1, 0, inventory.getSizeInventory(), false)) {
+//          return ItemStack.EMPTY;
+//        }
+//      }
+//      if (itemstack1.getCount() == 0) {
+//        slot.putStack(ItemStack.EMPTY);
+//      } else {
+//        slot.onSlotChanged();
+//      }
+//    }
+//    return itemstack;
+//  }
 
 }
