230ff89f1edd74c4c30d65b69cdfad4281a2b5a8
Unknown <mcpkmart@hotmail.nl>
2018-10-04 14:22:40 +0200

    Added particle effects

--- src/main/java/epicsquid/mysticallib/fx/EffectManager.java
+++ src/main/java/epicsquid/mysticallib/fx/EffectManager.java
@@ -3,6 +3,9 @@ package epicsquid.mysticallib.fx;
 import java.util.ArrayList;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -49,6 +52,37 @@ public class EffectManager {
   public void onRenderLast(RenderWorldLastEvent event) {
     for (int i = 0; i < effects.size(); i++) {
       effects.get(i).renderTotal(event.getPartialTicks());
+    }
+  }
+
+  public static void assignEffect(EntityLivingBase entity, String effect, int duration, NBTTagCompound data){
+    if (!(entity instanceof EntityPlayer)){
+      if (!entity.getEntityData().hasKey(Constants.EFFECT_TAG)){
+        entity.getEntityData().setTag(Constants.EFFECT_TAG, new NBTTagCompound());
+      }
+    }
+    else if (entity.hasCapability(PlayerDataProvider.playerDataCapability, null)){
+      if (!entity.getCapability(PlayerDataProvider.playerDataCapability, null).getData().hasKey(Constants.EFFECT_TAG)){
+        entity.getCapability(PlayerDataProvider.playerDataCapability, null).getData().setTag(Constants.EFFECT_TAG, new NBTTagCompound());
+        entity.getCapability(PlayerDataProvider.playerDataCapability, null).markDirty();
+      }
+    }
+    NBTTagCompound tag = null;
+    if (!(entity instanceof EntityPlayer)){
+      tag = entity.getEntityData().getCompoundTag(Constants.EFFECT_TAG);
+    }
+    else if (entity.hasCapability(PlayerDataProvider.playerDataCapability, null)){
+      tag = entity.getCapability(PlayerDataProvider.playerDataCapability, null).getData().getCompoundTag(Constants.EFFECT_TAG);
+    }
+    if (tag != null){
+      if (!tag.hasKey(effect)){
+        effects.get(effect).onApplied(entity, data);
+      }
+      tag.setInteger(effect, duration);
+      tag.setTag(effect+"_data", data);
+      if (entity.hasCapability(PlayerDataProvider.playerDataCapability, null)){
+        entity.getCapability(PlayerDataProvider.playerDataCapability, null).markDirty();
+      }
     }
   }
 }
