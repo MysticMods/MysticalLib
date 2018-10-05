a420731b63daf7850c15707792d182ab044b84a2
Unknown <mcpkmart@hotmail.nl>
2018-10-05 17:51:39 +0200

    Added a method for dropping items out of an ItemStackHandler

--- src/main/java/epicsquid/mysticallib/tile/TileBase.java
+++ src/main/java/epicsquid/mysticallib/tile/TileBase.java
@@ -4,9 +4,13 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import epicsquid.mysticallib.LibEvents;
+import epicsquid.mysticallib.network.MessageTEUpdate;
+import epicsquid.mysticallib.network.PacketHandler;
 import epicsquid.mysticallib.util.Util;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
@@ -15,6 +19,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class TileBase extends TileEntity implements ITile {
   public boolean dirty = false;
@@ -56,5 +61,18 @@ public class TileBase extends TileEntity implements ITile {
   @Nonnull
   public static String getTileName(@Nonnull Class<? extends TileEntity> teClass) {
     return Util.getLowercaseClassName(teClass);
+  }
+
+  protected boolean dropItemInInventory(ItemStackHandler inventory, int slot){
+    if (!inventory.getStackInSlot(slot).isEmpty()) {
+      ItemStack extracted = inventory.extractItem(slot, 1, false);
+      if (!world.isRemote) {
+        world.spawnEntity(new EntityItem(world, getPos().getX() + 0.5, getPos().getY() + 0.5, getPos().getZ() + 0.5, extracted));
+      }
+      markDirty();
+      PacketHandler.INSTANCE.sendToAll(new MessageTEUpdate(this.getUpdateTag()));
+      return true;
+    }
+    return false;
   }
 }
