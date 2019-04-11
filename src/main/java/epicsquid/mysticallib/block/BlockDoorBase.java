3233a280984352a4d13353a054dd885e9d6102d8
Jon McManus <due@wxwhatever.com>
2019-04-11 15:37:54 +1000

    BlockDoorBase.

---
+++ src/main/java/epicsquid/mysticallib/block/BlockDoorBase.java
@@ -0,0 +1,147 @@
+package epicsquid.mysticallib.block;
+
+import epicsquid.mysticallib.LibRegistry;
+import epicsquid.mysticallib.model.CustomModelBlock;
+import epicsquid.mysticallib.model.CustomModelLoader;
+import epicsquid.mysticallib.model.ICustomModeledObject;
+import epicsquid.mysticallib.model.IModeledObject;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDoor;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemDoor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class BlockDoorBase extends BlockDoor implements IBlock {
+  private final @Nonnull
+  Item itemBlock;
+  public List<ItemStack> drops = null;
+  private boolean isOpaque = false;
+  private boolean hasCustomModel = false;
+  private boolean isFlammable = false;
+  private BlockRenderLayer layer = BlockRenderLayer.SOLID;
+  private Block parent;
+  public String name = "";
+
+  public BlockDoorBase(@Nonnull Block base, @Nonnull SoundType type, float hardness, @Nonnull String name) {
+    super(base.getDefaultState().getMaterial());
+    this.parent = base;
+    this.name = name;
+    setCreativeTab(null);
+    setUnlocalizedName(name);
+    setRegistryName(name);
+    setSoundType(type);
+    setLightOpacity(0);
+    setHardness(hardness);
+    setOpacity(false);
+    this.fullBlock = false;
+    itemBlock = new ItemDoor(this).setUnlocalizedName(name).setRegistryName(LibRegistry.getActiveModid(), name);
+  }
+
+  @Nonnull
+  public BlockDoorBase setFlammable(boolean flammable) {
+    this.isFlammable = flammable;
+    return this;
+  }
+
+  @Override
+  @Nonnull
+  public BlockDoorBase setResistance(float resistance) {
+    super.setResistance(resistance);
+    return this;
+  }
+
+  @Nonnull
+  public BlockDoorBase setModelCustom(boolean custom) {
+    this.hasCustomModel = custom;
+    return this;
+  }
+
+  @Nonnull
+  public BlockDoorBase setHarvestReqs(String tool, int level) {
+    setHarvestLevel(tool, level);
+    return this;
+  }
+
+  @Nonnull
+  public BlockDoorBase setOpacity(boolean isOpaque) {
+    this.isOpaque = isOpaque;
+    return this;
+  }
+
+  @Nonnull
+  public BlockDoorBase setLayer(@Nonnull BlockRenderLayer layer) {
+    this.layer = layer;
+    return this;
+  }
+
+  @Override
+  public boolean isOpaqueCube(@Nonnull IBlockState state) {
+    return isOpaque;
+  }
+
+  public boolean hasCustomModel() {
+    return this.hasCustomModel;
+  }
+
+  @Override
+  public boolean isFullCube(@Nonnull IBlockState state) {
+    return false;
+  }
+
+  @Override
+  public boolean canPlaceTorchOnTop(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
+    return true;
+  }
+
+  @Override
+  public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
+    if (tab == this.getCreativeTabToDisplayOn()) {
+      list.add(new ItemStack(this, 1));
+    }
+  }
+
+  @Override
+  public boolean isFlammable(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing face) {
+    return isFlammable || super.isFlammable(world, pos, face);
+  }
+
+  @Override
+  public int getFlammability(IBlockAccess world, BlockPos pos, EnumFacing face) {
+    return isFlammable ? 100 : super.getFlammability(world, pos, face);
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  @Nonnull
+  public BlockRenderLayer getBlockLayer() {
+    return this.layer;
+  }
+
+  @Override
+  @Nonnull
+  public Item getItemBlock() {
+    return itemBlock;
+  }
+
+  @Nonnull
+  public Block getParent() {
+    return parent;
+  }
+}
