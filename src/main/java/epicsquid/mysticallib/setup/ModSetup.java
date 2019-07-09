3e2f41ae6a3c49c21e88abe905d356d46db9b170
EpicSquid <epicsquid315@gmail.com>
2019-07-09 20:12:23 +1000

    Added oregen

--- src/main/java/epicsquid/mysticallib/setup/ModSetup.java
+++ src/main/java/epicsquid/mysticallib/setup/ModSetup.java
@@ -1,8 +1,11 @@
 package epicsquid.mysticallib.setup;
 
+import epicsquid.mysticallib.world.GenerationRegistry;
+import epicsquid.mysticallib.world.OreGenerator;
+
 public class ModSetup {
 
-  public void init() {
-
-  }
+	public void init() {
+		GenerationRegistry.oreGen.forEach(OreGenerator::init);
+	}
 }
---
+++ src/main/java/epicsquid/mysticallib/world/DimensionalOreFeature.java
@@ -0,0 +1,30 @@
+package epicsquid.mysticallib.world;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationSettings;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+public class DimensionalOreFeature<F extends IFeatureConfig> extends ConfiguredFeature<F> {
+
+	private DimensionType dimType;
+
+	public DimensionalOreFeature(ConfiguredFeature<F> feature, DimensionType dimension) {
+		super(feature.feature, feature.config);
+		this.dimType = dimension;
+	}
+
+	@Override
+	public boolean place(@Nonnull IWorld world, @Nonnull ChunkGenerator<? extends GenerationSettings> chunk, @Nonnull Random rand, @Nonnull BlockPos pos) {
+		if (world.getDimension().getType().equals(dimType)) {
+			return super.place(world, chunk, rand, pos);
+		}
+		return false;
+	}
+}
