6779f142eb453b0157122fa3ac95682edb2c1672
Jon McManus <due@wxwhatever.com>
2019-10-25 15:07:28 +1000

    Slight tweaks.

--- src/main/java/epicsquid/mysticallib/item/KnifeItem.java
+++ src/main/java/epicsquid/mysticallib/item/KnifeItem.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
 import net.minecraft.item.ToolItem;
 
 import java.util.Set;
@@ -12,7 +13,7 @@ public class KnifeItem extends ToolItem {
   public static Set<Block> EFFECTIVE_BLOCKS = Sets.newHashSet(Blocks.ACACIA_LOG, Blocks.BIRCH_LOG, Blocks.DARK_OAK_LOG, Blocks.JUNGLE_LOG, Blocks.OAK_LOG, Blocks.SPRUCE_LOG);
 
   // TODO rework knives to strip logs of bark with right click, or drop bark by mining it
-  public KnifeItem(IItemTier tier, float attackDamage, float attackSpeed, Properties props) {
+  public KnifeItem(IItemTier tier, float attackDamage, float attackSpeed, Item.Properties props) {
     super(attackDamage, attackSpeed, tier, EFFECTIVE_BLOCKS, props);
   }
 }
