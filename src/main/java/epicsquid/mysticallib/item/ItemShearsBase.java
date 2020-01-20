1f15a9ac59c6de8ce39019a9487354a31c396fd2
Jon McManus <due@wxwhatever.com>
2020-01-20 14:55:02 +1000

    ItemShearsBase.

---
+++ src/main/java/epicsquid/mysticallib/item/ItemShearsBase.java
@@ -0,0 +1,42 @@
+package epicsquid.mysticallib.item;
+
+import epicsquid.mysticallib.LibRegistry;
+import epicsquid.mysticallib.model.CustomModelItem;
+import epicsquid.mysticallib.model.CustomModelLoader;
+import epicsquid.mysticallib.model.ICustomModeledObject;
+import epicsquid.mysticallib.model.IModeledObject;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.ItemDoor;
+import net.minecraft.item.ItemShears;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+
+public class ItemShearsBase extends ItemShears implements ICustomModeledObject, IModeledObject {
+
+  private boolean hasCustomModel;
+
+  public ItemShearsBase(String name) {
+    super();
+    setTranslationKey(name);
+    setRegistryName(LibRegistry.getActiveModid(), name);
+  }
+
+  public ItemShearsBase setModelCustom(boolean custom) {
+    this.hasCustomModel = custom;
+    return this;
+  }
+
+  @Override
+  public void initModel() {
+    ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "handlers"));
+  }
+
+  @Override
+  public void initCustomModel() {
+    if (this.hasCustomModel) {
+      CustomModelLoader.itemmodels
+          .put(getRegistryName(), new CustomModelItem(false, new ResourceLocation(getRegistryName().getNamespace() + ":items/" + getRegistryName().getPath())));
+    }
+  }
+}
