d1027912e1043ecfb775cd35a0668cdd7d1fe1d5
Jon McManus <due@wxwhatever.com>
2020-04-28 12:06:44 +1000

    ItemArmorBase.

--- src/main/java/epicsquid/mysticallib/material/MaterialTypes.java
+++ src/main/java/epicsquid/mysticallib/material/MaterialTypes.java
@@ -4,24 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 
 public class MaterialTypes {
   public static Map<String, Item.ToolMaterial> materialMap = new HashMap<>();
+  public static Map<String, ItemArmor.ArmorMaterial> armorMaterialMap = new HashMap<>();
   public static Map<String, KnifeStats> statsMap = new HashMap<>();
   public static Map<Item.ToolMaterial, KnifeStats> materialToStatsMap = new HashMap<>();
 
-  public static void addMaterial(String name, Item.ToolMaterial material, float damage, float speed) {
-    addMaterial(name, material, new KnifeStats(damage, speed));
+  public static void addMaterial(String name, Item.ToolMaterial material, ItemArmor.ArmorMaterial armor, float damage, float speed) {
+    addMaterial(name, material, armor, new KnifeStats(damage, speed));
   }
 
-  public static void addMaterial(String name, Item.ToolMaterial material, KnifeStats stats) {
+  public static void addMaterial(String name, Item.ToolMaterial material, ItemArmor.ArmorMaterial armor, KnifeStats stats) {
     materialToStatsMap.put(material, stats);
+    armorMaterialMap.put(name, armor);
     statsMap.put(name, stats);
     materialMap.put(name, material);
   }
 
   public static Item.ToolMaterial material(String name) {
     return materialMap.get(name);
+  }
+
+  public static ItemArmor.ArmorMaterial armor (String name) {
+    return armorMaterialMap.get(name);
   }
 
   public static KnifeStats stats(String name) {
