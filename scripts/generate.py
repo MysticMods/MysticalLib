fd520b986eda0d6c8a8778e89c9ed8b416516382
Jon McManus <due@wxwhatever.com>
2020-11-08 20:16:14 +1000

    Tweak scripts for wall generation.

--- scripts/generate.py
+++ scripts/generate.py
@@ -23,39 +23,68 @@ def main (args):
 
     directory = data["directory"]
     texture_base = data["texture_base"]
+    blocks = data["blocks"]
+    items = data["items"]
 
     for model in data["models"]:
         base = model["base"]
         texture = model.get("texture", base)
         type = model["type"].lower()
+        this_texture = texture_base + texture
+
 
         if type not in TYPES:
             print("Invalid type: " + type)
             continue
 
-        this_template = os.path.join(template_path, type + ".json")
-        if not os.path.exists(this_template):
-            print("Invalid template or template does not exist: " + this_template)
+        if type == "wall":
+            wall_template = open(os.path.join(template_path, "wall.json")).read()
+            wall_inventory = open(os.path.join(template_path, "wall_inventory.json")).read()
+            wall_inventory_item = open(os.path.join(template_path, "wall_inventory_item.json")).read()
+            wall_side = open(os.path.join(template_path, "wall_side.json")).read()
+            wall_post = open(os.path.join(template_path, "wall_post.json")).read()
+            filename = os.path.join(directory, base + "_wall.json")
 
-        with open(this_template) as o:
-            template_data = o.read()
+            this_texture2 = texture_base + texture
+            with open(os.path.join(blocks, base + "_wall_inventory.json"), "w") as o:
+                o.write(wall_inventory % tuple([this_texture2, ] * wall_inventory.count("%s")))
 
-        this_texture = texture_base + texture
+            with open(os.path.join(blocks, base + "_wall_side.json"), "w") as o:
+                o.write(wall_side % tuple([this_texture2, ] * wall_side.count("%s")))
 
-        if type == "log" or type == "runed_wood":
-            texture_params = tuple([model["log"], this_texture])
+
+            with open(os.path.join(blocks, base + "_wall_post.json"), "w") as o:
+                o.write(wall_post % tuple([this_texture2, ] * wall_post.count("%s")))
+
+            this_texture2 = texture_base.replace("blocks/", "") + base
+            with open(filename, "w") as o:
+                o.write(wall_template % tuple([this_texture2, ] * wall_template.count("%s")))
+
+            this_texture2 = texture_base.replace("blocks", "block") + base
+            with open(os.path.join(items, base + "_wall.json"), "w") as o:
+                o.write(wall_inventory_item % tuple([this_texture2, ] * wall_inventory_item.count("%s")))
         else:
-            texture_params = tuple([this_texture, ] * template_data.count("%s"))
+            this_template = os.path.join(template_path, type + ".json")
+            if not os.path.exists(this_template):
+                print("Invalid template or template does not exist: " + this_template)
 
-        if type == "block" or type == "item" or type == "tool" or type == "log" or type == "runed_wood":
-            filename = os.path.join(directory, base + ".json")
-        else:
-            filename = os.path.join(directory, base + "_" + type + ".json")
+            with open(this_template) as o:
+                template_data = o.read()
 
-        print("Attempting to create " + filename)
+            if type == "log" or type == "runed_wood":
+                texture_params = tuple([model["log"], this_texture])
+            else:
+                texture_params = tuple([this_texture, ] * template_data.count("%s"))
 
-        with open(filename, "w") as o:
-            o.write(template_data % texture_params)
+            if type == "block" or type == "item" or type == "tool" or type == "log" or type == "runed_wood":
+                filename = os.path.join(directory, base + ".json")
+            else:
+                filename = os.path.join(directory, base + "_" + type + ".json")
+
+            print("Attempting to create " + filename)
+
+            with open(filename, "w") as o:
+                o.write(template_data % texture_params)
 
 
 if __name__=="__main__":
